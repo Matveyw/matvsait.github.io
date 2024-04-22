2f9423eaa2f125aab3dae5331f91726ec5ba0068
Matveyw <152613854+Matveyw@users.noreply.github.com>
2024-04-22 12:46:45 +0500

    Add files via upload

--- Java.js
+++ Java.js
@@ -1,4 +1,48 @@
-let isRain = confirm('Вы подтверждаете свои действия на странице?')
-let rain = confirm('Вы подтверждаете установку сторонних программ на свой ПК?')
-let Rain = alert('Спасибо за Вашу помощь! Программа была успешно установлена.')
-let rain = prompt('Вы довольны?')
+let f = document.querySelector('.top-form');
+
+const fff = function(evt) {
+	let a = 'color-' + evt.target.value;
+	document.querySelector('main').classList.remove('color-white');
+	document.querySelector('main').classList.remove('color-black');
+	document.querySelector('main').classList.remove('color-rgb');
+	document.querySelector('main').classList.add(a);
+};
+
+addEventListener('change', fff);
+
+
+
+
+
+
+			<form class="top-form">
+			<input type="radio" value="white" name="tema" checked="checked"/>Светлая
+			<input type="radio" value="black" name="tema"/>Тёмная
+			<input type="radio" value="rgb" name="tema"/>Авторская
+			</form>
+			
+			
+			
+			
+			
+			
+			
+			.top-form {
+	background-color: #222222;
+}
+
+.color-white {
+	background-color: black;
+	color: black;
+}
+
+.color-black {
+	background-color: black;
+	color: black;
+}
+
+
+.color-rgb {
+	background-color: green;
+	color: #000000;
+}
